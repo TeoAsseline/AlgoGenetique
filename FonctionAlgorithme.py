285575e050f08809be39a8e6e3e3ff39710b3bd3
ArthurWdnr <121061305+ArthurWdnr@users.noreply.github.com>
2023-09-14 12:01:40 +0200

    classement initial crée

--- FonctionAlgorithme.py
+++ FonctionAlgorithme.py
@@ -14,7 +14,36 @@ def creerMatrice(n):
     # Remplacer la diagonale par des zéros
     np.fill_diagonal(matrice, 0)
     
-    # Afficher la matrice résultante
-    print(matrice)
+    for i in range(n):
+        for j in range(i, n):
+            matrice[j][i] = matrice[i][j]
     
-creerMatrice(5)
+    # Afficher la matrice résultante
+    return matrice
+    
+def creerIndividusDepart(n):
+    liste = list(range(0, n))
+    np.random.shuffle(liste)
+    
+    return liste
+    
+def calculerDistance(M,I,n):
+    distance = 0
+    for i in range(n):
+        depart = I[i]
+        if (i==n-1):
+            arriver = I[0]
+        else:
+            arriver = I[i+1]
+        distance += M[depart,arriver]
+        
+    return(distance)
+    
+M = creerMatrice(5)
+classement = []
+for i in range(5):
+    I = creerIndividusDepart(5)
+    classement.append((I,calculerDistance(M, I, 5)))
+tableau_trie = sorted(classement, key=lambda x: x[1])
+print(tableau_trie)
+print(tableau_trie[2])
