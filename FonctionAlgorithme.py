db6cd37bdd667ac6f71fc1b22e492dd96c4811a3
ArthurWdnr <121061305+ArthurWdnr@users.noreply.github.com>
2023-09-14 11:07:37 +0200

    oui
    Signed-off-by: ArthurWdnr <121061305+ArthurWdnr@users.noreply.github.com>

--- FonctionAlgorithme.py
+++ FonctionAlgorithme.py
@@ -4,6 +4,17 @@ Created on Thu Sep 14 00:34:39 2023
 
 @author: assel
 """
+import numpy as np
+
 def creerMatrice(n):
     
-    return 2
+    # Générer une matrice aléatoire de taille n x n
+    matrice = np.random.randint(1, 100, size =(n,n))
+    
+    # Remplacer la diagonale par des zéros
+    np.fill_diagonal(matrice, 0)
+    
+    # Afficher la matrice résultante
+    print(matrice)
+    
+creerMatrice(5)
